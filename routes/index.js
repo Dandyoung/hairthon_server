e8626f56bbf4666d79b6d168724d95d9cad56842
youngwoo <eoduq6464@naver.com>
2023-07-29 15:30:43 +0900

    서버통신test

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/test', function(req, res, next) {
   // localhos:3000/ 링크로 get 요청 들어오면 axios 실행 -> flask 실행 서버인 5000포트 /api/flask url로 데이터 요청
   // app.py에서 json 데이터를 리턴받음
   // axios? http 비동기 통신 라이브러리
-  axios.get('http://127.0.0.1:5000/api/flask')
+  axios.get('https://port-0-hairthon-server-rt92alkjpc0lh.sel4.cloudtype.app/')
     .then(response => {
       const data = response.data;
       // JSON 데이터를 클라이언트로 전달
